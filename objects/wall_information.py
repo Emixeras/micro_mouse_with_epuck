1334ddd011086a54b1899ad66e09d01b0dfaaaca
Felix Hagenbrock <felix_hendrik_josef.hagenbrock@smail.th-koeln.de>
2024-12-05 10:23:44 +0100

    add toString

--- objects/wall_information.py
+++ objects/wall_information.py
@@ -4,3 +4,6 @@ class WallInformation:
         self.back = back
         self.left = left
         self.right = right
+
+    def __str__(self):
+        return f"WallInformation(front={self.front}, back={self.back}, left={self.left}, right={self.right})"
